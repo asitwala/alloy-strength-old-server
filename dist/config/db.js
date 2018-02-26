771bb43c05ee4cf06932ae86df624b16f91ca570
Amy Sitwala <asitwala@Amys-MacBook-Pro.local>
2018-02-25 21:20:43 -0600

    Fixing connection

--- dist/config/db.js
+++ dist/config/db.js
@@ -4,5 +4,5 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.default = {
-    db: 'mongodb://asitwala:skiergrL4*@alloystrength-mongo-shard-00-00-z0yra.mongodb.net:27017,alloystrength-mongo-shard-00-01-z0yra.mongodb.net:27017,alloystrength-mongo-shard-00-02-z0yra.mongodb.net:27017/alloystrength-db?ssl=true&replicaSet=AlloyStrength-Mongo-shard-0&authSource=admin'
+    db: 'mongodb://asitwala:alloystrength123*()@alloystrength-mongo-shard-00-00-z0yra.mongodb.net:27017,alloystrength-mongo-shard-00-01-z0yra.mongodb.net:27017,alloystrength-mongo-shard-00-02-z0yra.mongodb.net:27017/alloystrength-db?ssl=true&replicaSet=AlloyStrength-Mongo-shard-0&authSource=admin'
 };
---
+++ dist/ecosystem.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  apps: [{
+    name: 'alloy-strength-server',
+    script: './src/app.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-197-11-241.compute-1.amazonaws.com',
+      key: '~/.ssh/AlloyStrengthKeyPair.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:asitwala/alloy-strength-server.git',
+      path: '/home/ubuntu/alloy-strength-server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
